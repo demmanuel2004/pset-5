8b651d5513ee6f5166ac3c16dfd9086bd15bf141
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-12-06 20:13:38 -0500

    Update shapes.js

--- js/shapes.js
+++ js/shapes.js
@@ -1,31 +1,32 @@
 window.onload = function() {
     document.getElementById("hello-sample").onclick = sayHelloStaff;
     document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
-    document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
-    document.getElementById("triangle-sample").onclick = drawTriangleStaff;
-    document.getElementById("smile-sample").onclick = drawFaceStaff;
-    document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
+document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
+document.getElementById("triangle-sample").onclick = drawTriangleStaff;
+document.getElementById("smile-sample").onclick = drawFaceStaff;
+document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
 
-    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
-    //
-    // document.getElementById("some-id")   <-- you need to give each button a unique ID
-    //                                          and access it in this manner
-    //
-    // onclick is an event that is fired when you click something (in our case, a button).
-    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
-    // you should set onclick equal to your function names (i.e., sayHello).
-    //
-    // there are six event listeners being added for the staff solutions. you'll have an
-    // equivalent set of six event listeners for your solutions. the first one is done for you.
+// this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
+//
+// document.getElementById("some-id") <-- you need to give each button a unique ID
+// and access it in this manner
+//
+// onclick is an event that is fired when you click something (in our case, a button).
+// when we give onclick a value, we're telling JavaScript what to do when we click the button.
+// you should set onclick equal to your function names (i.e., sayHello).
+//
+// there are six event listeners being added for the staff solutions. you'll have an
+// equivalent set of six event listeners for your solutions. the first one is done for you.
 
-    document.getElementById("hello").onclick = sayHello;
-   
+document.getElementById("hello").onclick = sayHello;
+document.getElementById("rectangle").onclick = drawRectangle;
+document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
+document.getElementById("triangle").onclick = drawTriangle;
 }
 
 /*
- * Exercise 1.
- */
-
+* Exercise 1.
+*/
 
 const sayHello = function () {
 
