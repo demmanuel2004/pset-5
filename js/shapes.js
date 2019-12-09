109b716362fc83fb8399a4687e96a5ac9042868b
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-12-09 12:38:07 -0500

    Update shapes.js

--- js/shapes.js
+++ js/shapes.js
@@ -23,6 +23,7 @@ document.getElementById("hello").onclick = sayHello;
  document.getElementById("rectangle").onclick = drawRectangle;
 document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
 document.getElementById("triangle").onclick = drawTriangle;
+document.getElementById("smile").onclick = drawSmile;
 }
 
 /*
@@ -169,7 +170,23 @@ const drawTriangle = function() {
  */
 
 const drawFace = function() {
-    // write your exercise 4 code here
+   
+ function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+     var ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.stroke();
+  }
+}
 };
 
 /*
